e04d843b5955db3e173199a4a96e28b9764290b8
Maxou <max.allemeersch@gmail.com>
2024-01-27 23:30:06 +0100

    feat() : Basic save informations

--- Assets/Scripts/PlayerLevel.cs
+++ Assets/Scripts/PlayerLevel.cs
@@ -10,23 +10,35 @@ using UnityEngine.UI;
 public class PlayerLevel : MonoBehaviour
 {
     [SerializeField] private float playerXp = 0;
-    [SerializeField] private int playerLevel = 1;
+    [SerializeField] private int playerLevelValue = 1;
     [SerializeField] private float playerNeededXp = 100;
     private float experienceMultiplier = .50f;
 
     [SerializeField] private Slider xpSlider;
     [SerializeField] private Text playerLvlText;
 
+    public float PlayerXp 
+    { 
+        get { return playerXp; }
+        set { playerXp = value; }
+    }
 
     public int PlayerLevelValue
     {
-        get { return playerLevel; }
-        set { playerLevel = value; }
+        get { return playerLevelValue; }
+        set { playerLevelValue = value; }
     }
 
     public float PlayerNeededXp
     {
         get { return playerNeededXp; }
+        set { playerNeededXp = value; }
+    }
+
+    public float ExperienceMultiplier 
+    { 
+        get { return experienceMultiplier; }
+        set { experienceMultiplier = value; }
     }
 
 
@@ -39,7 +51,7 @@ public class PlayerLevel : MonoBehaviour
 
     public void UpdatePlayerLevelUI()
     {
-        playerLvlText.text = $"Player Level : {playerLevel}";
+        playerLvlText.text = $"Player Level : {playerLevelValue}";
     }    
 
     public void UpdateXpSlider()
@@ -52,7 +64,7 @@ public class PlayerLevel : MonoBehaviour
     {
         if (playerXp >= playerNeededXp)
         {
-            playerLevel++;
+            playerLevelValue++;
             playerXp = 0;
             playerNeededXp += playerNeededXp * experienceMultiplier;
             experienceMultiplier += 0.50f;
